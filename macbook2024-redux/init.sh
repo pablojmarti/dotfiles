5c20739f97fdacca07f1d5b9d40097e3e956efea
pablojmarti <pablo.marticordero@10up.com>
2025-10-24 12:31:15 -0400

    fix init

--- macbook2024-redux/init.sh
+++ macbook2024-redux/init.sh
@@ -6,11 +6,23 @@
 
 # Adding this command here to document how I sync'd my actual nvim config with this repo
 # find /Users/pablo/workspace/projects/dotfiles/macbook2024-redux/.config/nvim/ -type d -exec ln -s '{}' /Users/pablo/.config/nvim  \;
-for file in $( find $(pwd) -name ".*" -type f ); 
+# for file in $( find $(pwd) -name ".*" -type f ); 
+# do
+#   linking_path="$HOME/$(basename $file)"
+#   echo "Linking $file with $linking_path"
+#   ln -fs $file $HOME
+# done
+
+for folder in $(find .config -type d);
 do
-  linking_path="$HOME/$(basename $file)"
-  echo "Linking $file with $linking_path"
-  ln -fs $file $HOME
+  echo "mkdir -p $HOME/$folder"
+  mkdir -p $HOME/$folder
+  for file in $(find $folder -maxdepth 1 -type f);
+  do
+    echo "ln -s $(pwd)/$file $HOME/$file"
+    ln -s $(pwd)/$file $HOME/$file
+  done
 done
 
 echo "Config done"
+
